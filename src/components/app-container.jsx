6b4f525a8134e4d8ba2b4b9e8cbffbed84371fa9
NaufalAjaSih <masanaufal@gmail.com>
2023-01-31 11:14:13 +0700

    first commit

---
+++ src/components/app-container.jsx
@@ -0,0 +1,16 @@
+import Footer from "../layouts/Footer";
+import Header from "../layouts/Header";
+import Body from "../layouts/Body";
+
+export function AppContiner() {
+  return (
+    <>
+      {/* Header */}
+      <Header />
+      {/* Body */}
+      <Body />
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#423F71",
+        header: "#292841",
+        body: "#1C1B29",
+      },
+    },
+  },
+  plugins: [],
+};
